c11488ccb31470b21e1a2fa74226fa86866baa69
Jonathan Branam <jonathan.branam@gmail.com>
2018-11-23 11:46:03 -0500

    Update check script to find and install missing dependencies.

--- bin/check.sh
+++ bin/check.sh
@@ -1,6 +1,49 @@
 #!/usr/bin/env bash
 
-command -v tmux >/dev/null 2>&1 || {
-  printf >&2 "tmux is required to use this program. You can install tmux using the command:\napt-get install -y tmux\nAborting.\n"; exit 1;
-}
+# List of dependencies to check for
+dependencies=(tmux fake-thing something-else i-made_this_up)
+# dependencies=(tmux git vim)
+
+# Check for dependencies and abort if not installed
+missing_deps=()
+for dep in "${dependencies[@]}"; do
+  command -v "$dep" >/dev/null 2>&1 || {
+    missing_deps+=("$dep")
+  }
+done
+
+# Simple getopts setup. Replace or expand as needed. This script only expects
+# the install_deps variable set to any value to signal to install dependencies.
+while getopts ":i" o; do
+    case "${o}" in
+        i)
+            install_deps="1"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Install missing dependencies using apt-get. This may not be appropriate for
+# all dependencies so please update this if that is the case!
+if [ ! -z $install_deps ]; then
+  if [ ${#missing_deps[@]} -ne 0 ]; then
+    echo apt-get install -y "${missing_deps[@]}"
+    echo Dependencies installed. Please run the script again without the -i option.
+  else
+    echo No missing dependencies detected. Please run the script again without the -i option.
+  fi
+  exit 0
+fi
+
+if [ ${#missing_deps[@]} -ne 0 ]; then
+  printf >&2 "Missing dependencies:\\n"
+  printf >&2 "    %s\\n" "${missing_deps[@]}"
+  printf >&2 "You can install them using the command:\\n$0 -i\\nAborting.\\n"
+  exit 1
+fi
+
+
 
--- bin/test-getopts.sh
+++ bin/test-getopts.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+# Simple example script for bash built-in getopts
 
 usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
 
-while getopts ":s:p:" o; do
+while getopts ":s:p:i" o; do
     case "${o}" in
         s)
             s=${OPTARG}
             ((s == 45 || s == 90)) || usage
+            ;;
+        i)
+            i_was_set="1"
             ;;
         p)
             p=${OPTARG}
@@ -24,3 +28,10 @@ fi
 
 echo "s = ${s}"
 echo "p = ${p}"
+
+
+if [ ! -z $i_was_set ]; then
+  echo "-i was set"
+else
+  echo "-i was not set"
+fi
